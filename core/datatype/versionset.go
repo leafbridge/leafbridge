257ab98f230fee11f08081f4f499dbbc8bf4cdbb
Joshua Sjoding <ga@josh.hey.nu>
2025-08-17 00:56:08 -0700

    core/datatype: Added VersionSet type

---
+++ core/datatype/versionset.go
@@ -0,0 +1,79 @@
+package datatype
+
+import "slices"
+
+// VersionSet is a set of versions.
+//
+// The set always stores versions in canonical form. It prevents duplicates
+// from being added that would be considered equivalent by [CompareVersions].
+type VersionSet map[Version]struct{}
+
+// Contains returns true if the given version is present in the set.
+func (set VersionSet) Contains(version Version) bool {
+	version = version.Canonical()
+
+	// Fast path for exact matches.
+	_, present := set[version]
+	if present {
+		return true
+	}
+
+	// Slow path for inexact matches.
+	for existing := range set {
+		if CompareVersions(version, existing) == 0 {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Add adds the given version to the set. If it is already present, it takes
+// no action.
+func (set VersionSet) Add(version Version) {
+	version = version.Canonical()
+
+	if set.Contains(version) {
+		return
+	}
+
+	set[version] = struct{}{}
+}
+
+// Remove removes the given version from the set. If it is not present, it
+// takes no action.
+func (set VersionSet) Remove(version Version) {
+	version = version.Canonical()
+
+	// Fast path for exact matches.
+	_, present := set[version]
+	if present {
+		delete(set, version)
+		return
+	}
+
+	// Slow path for inexact matches.
+	for existing := range set {
+		if CompareVersions(version, existing) == 0 {
+			delete(set, existing)
+			return
+		}
+	}
+}
+
+// List returns the members of the set in an ordered list from lowest
+// to highest.
+func (set VersionSet) List() (versions []Version) {
+	if len(set) == 0 {
+		return
+	}
+
+	versions = make([]Version, 0, len(set))
+	for version := range set {
+		versions = append(versions, version)
+	}
+
+	slices.SortFunc(versions, CompareVersions)
+
+	return
+}
---
+++ core/datatype/versionset_test.go
@@ -0,0 +1,42 @@
+package datatype_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/leafbridge/leafbridge/core/datatype"
+)
+
+func TestVersionSet(t *testing.T) {
+	set := make(datatype.VersionSet)
+	set.Add("1")
+	set.Add("1")
+	set.Add("1.0")
+	set.Add("1.0.0")
+	set.Remove("1.0")
+	if set.Contains("1") {
+		t.Fatalf("the set should not contain the version \"1\"")
+	}
+	if len(set.List()) != 0 {
+		t.Fatalf("the set should be empty")
+	}
+	set.Add("1.0")
+	set.Remove("1.0")
+	set.Add("2")
+	set.Add("1")
+	if !set.Contains("2") || !set.Contains("2.0.0.0.0") {
+		t.Fatalf("the set should contain the version \"2\"")
+	}
+	set.Add("5.2.1")
+	set.Add("27.2")
+	set.Add("2.2.1.5")
+	var values []string
+	for _, version := range set.List() {
+		values = append(values, string(version))
+	}
+	result := strings.Join(values, ", ")
+	const expected = "1, 2, 2.2.1.5, 5.2.1, 27.2"
+	if result != expected {
+		t.Fatalf("the resulting set did not contain the expected values: got \"%s\" (wanted \"%s\")", result, expected)
+	}
+}
