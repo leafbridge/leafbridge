c459cc45816aaabc0ab945680eac63a9eda44508
Joshua Sjoding <ga@josh.hey.nu>
2025-08-16 18:30:53 -0700

    core/datatype: Added VersionRange type

---
+++ core/datatype/versionrange.go
@@ -0,0 +1,155 @@
+package datatype
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// VersionRange describes a range of versions.
+//
+// It handles special cases in the following ways:
+//   - If Start is empty, it matches all versions before End.
+//   - If End is empty, it matches all versions after Start.
+//   - If both Start and End are empty, it matches all versions.
+//
+// The starting and ending versions can be inclusive or exclusive.
+//
+// When marshaled as JSON, it marshals exclusive Start fields as "after"
+// and exclusive End fields as "before". This allows the JSON representation
+// to be more inuitive.
+type VersionRange struct {
+	Start            Version
+	End              Version
+	StartIsExclusive bool
+	EndIsExclusive   bool
+}
+
+// versionRangeEncoding is used to encode and decode a version range as JSON.
+//
+// The "Start" and "After" fields are mutually exclusive, as are the "Before"
+// and "End" fields.
+type versionRangeEncoding struct {
+	Start  Version `json:"start,omitempty"`  // Inclusive start
+	After  Version `json:"after,omitempty"`  // Exclusive start
+	Before Version `json:"before,omitempty"` // Exclusive end
+	End    Version `json:"end,omitempty"`    // Inclusive end
+}
+
+// Includes returns true if the version range includes v.
+func (r VersionRange) Includes(v Version) bool {
+	if r.Start != "" {
+		switch CompareVersions(v, r.Start) {
+		case 0:
+			if r.StartIsExclusive {
+				return false
+			}
+		case -1:
+			return false
+		}
+	}
+	if r.End != "" {
+		switch CompareVersions(v, r.End) {
+		case 0:
+			if r.EndIsExclusive {
+				return false
+			}
+		case 1:
+			return false
+		}
+	}
+	return true
+}
+
+// String returns a string representation of the version range in one of
+// the following forms, with "(" and ")" denoting inclusive ranges and
+// "[" and "]" denoting exclusive ranges:
+//   - [Start, End]
+//   - (Start, End]
+//   - (Start, End)
+//   - [Start, End)
+//   - >Start
+//   - >=Start
+//   - <=End
+//   - <End
+//
+// If the range is empty, it returns the empty string.
+func (r VersionRange) String() string {
+	switch {
+	case r.Start != "" && r.End != "":
+		switch {
+		case r.StartIsExclusive && r.EndIsExclusive:
+			return "[" + r.Start.Canonical() + ", " + r.End.Canonical() + "]"
+		case !r.StartIsExclusive && !r.EndIsExclusive:
+			return "(" + r.Start.Canonical() + ", " + r.End.Canonical() + ")"
+		case !r.StartIsExclusive && r.EndIsExclusive:
+			return "(" + r.Start.Canonical() + ", " + r.End.Canonical() + "]"
+		default:
+			return "[" + r.Start.Canonical() + ", " + r.End.Canonical() + ")"
+		}
+	case r.Start != "":
+		if r.StartIsExclusive {
+			return ">" + string(r.Start.Canonical())
+		}
+		return ">=" + string(r.Start.Canonical())
+	case r.End != "":
+		if r.EndIsExclusive {
+			return "<" + string(r.End.Canonical())
+		}
+		return "<=" + string(r.End.Canonical())
+	default:
+		return ""
+	}
+}
+
+// UnmarshalJSON attempts to unmarshal the given JSON data into r.
+func (r *VersionRange) UnmarshalJSON(b []byte) error {
+	var aux versionRangeEncoding
+	if err := json.Unmarshal(b, &aux); err != nil {
+		return err
+	}
+
+	if aux.Start != "" && aux.After != "" {
+		return fmt.Errorf("version range contains both \"start\" and \"after\" fields, which are mutually exclusive (\"%s\" and \"%s\")", aux.Start, aux.After)
+	}
+
+	if aux.Before != "" && aux.End != "" {
+		return fmt.Errorf("version range contains both \"before\" and \"end\" fields, which are mutually exclusive (\"%s\" and \"%s\")", aux.Before, aux.End)
+	}
+
+	if aux.Start != "" {
+		r.Start = aux.Start
+	}
+	if aux.After != "" {
+		r.Start = aux.After
+		r.StartIsExclusive = true
+	}
+	if aux.Before != "" {
+		r.End = aux.Before
+		r.EndIsExclusive = true
+	}
+	if aux.End != "" {
+		r.End = aux.End
+	}
+
+	return nil
+}
+
+// MarshalJSON marshals the version range as JSON data.
+func (v VersionRange) MarshalJSON() ([]byte, error) {
+	var aux versionRangeEncoding
+	if v.Start != "" {
+		if v.StartIsExclusive {
+			aux.After = v.Start
+		} else {
+			aux.Start = v.Start
+		}
+	}
+	if v.End != "" {
+		if v.EndIsExclusive {
+			aux.Before = v.End
+		} else {
+			aux.End = v.End
+		}
+	}
+	return json.Marshal(aux)
+}
---
+++ core/datatype/versionrange_test.go
@@ -0,0 +1,109 @@
+package datatype_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+
+	"github.com/leafbridge/leafbridge/core/datatype"
+)
+
+type versionRangeFixture struct {
+	Version       datatype.Version
+	RangeString   string
+	MarshaledJSON string
+	Range         datatype.VersionRange
+	Included      bool
+}
+
+var versionRangeFixtures = []versionRangeFixture{
+	{Version: "1.0.0", RangeString: "", Range: datatype.VersionRange{}, Included: true},
+	{Version: "1.0.0", RangeString: ">=0.9.0", Range: datatype.VersionRange{Start: "0.9.0"}, Included: true},
+	{Version: "1.0.0", RangeString: ">=1.0.0", Range: datatype.VersionRange{Start: "1.0.0"}, Included: true},
+	{Version: "1.0.0", RangeString: ">1.0.0", Range: datatype.VersionRange{Start: "1.0.0", StartIsExclusive: true}, Included: false},
+	{Version: "1.0.0", RangeString: "<=1.0.1", Range: datatype.VersionRange{End: "1.0.1"}, Included: true},
+	{Version: "1.0.0", RangeString: "<=1.0.0", Range: datatype.VersionRange{End: "1.0.0"}, Included: true},
+	{Version: "1.0.0", RangeString: "<1.0.0", Range: datatype.VersionRange{End: "1.0.0", EndIsExclusive: true}, Included: false},
+	{Version: "0.5.2", RangeString: "(0.1.8, 0.7)", Range: datatype.VersionRange{Start: "0.1.8", End: "0.7"}, Included: true},
+	{Version: "0.5.2", RangeString: "(0.4, 8.25.1.3)", Range: datatype.VersionRange{Start: "0.4", End: "8.25.1.3"}, Included: true},
+	{Version: "1.0.0", RangeString: "(0.0, 0.9)", Range: datatype.VersionRange{Start: "0.0", End: "0.9"}, Included: false},
+	{Version: "0.1.1", RangeString: "(0.2, 9)", Range: datatype.VersionRange{Start: "0.2", End: "9"}, Included: false},
+	{Version: "1.0.0", RangeString: "(1.0.0, 1.2)", Range: datatype.VersionRange{Start: "1.0.0", End: "1.2"}, Included: true},
+	{Version: "1.0.0", RangeString: "[1.0.0, 1.2)", Range: datatype.VersionRange{Start: "1.0.0", StartIsExclusive: true, End: "1.2"}, Included: false},
+	{Version: "1.2.0", RangeString: "(1.0.0, 1.2)", Range: datatype.VersionRange{Start: "1.0.0", End: "1.2"}, Included: true},
+	{Version: "1.2.0", RangeString: "(1.0.0, 1.2]", Range: datatype.VersionRange{Start: "1.0.0", End: "1.2", EndIsExclusive: true}, Included: false},
+	{Version: "2", RangeString: "[1, 3]", Range: datatype.VersionRange{Start: "1", StartIsExclusive: true, End: "3", EndIsExclusive: true}, Included: true},
+	{Version: "2", RangeString: "[1, 2]", Range: datatype.VersionRange{Start: "1", StartIsExclusive: true, End: "2", EndIsExclusive: true}, Included: false},
+	{Version: "2", RangeString: "[2, 3]", Range: datatype.VersionRange{Start: "2", StartIsExclusive: true, End: "3", EndIsExclusive: true}, Included: false},
+	{Version: "2", RangeString: "[2, 2]", Range: datatype.VersionRange{Start: "2", StartIsExclusive: true, End: "2", EndIsExclusive: true}, Included: false},
+	{Version: "1.2.0", RangeString: "[1.2.0, 1.2]", Range: datatype.VersionRange{Start: "1.2.0", StartIsExclusive: true, End: "1.2", EndIsExclusive: true}, Included: false},
+}
+
+func TestVersionRange(t *testing.T) {
+	for i, fixture := range versionRangeFixtures {
+		t.Run(fmt.Sprintf("%d:%s:%s", i, fixture.RangeString, fixture.Version), func(t *testing.T) {
+			{
+				result := fixture.Range.Includes(fixture.Version)
+				if result != fixture.Included {
+					t.Fatalf("version range inclusion test doesn't match the expected result: got %t (wanted %t)", result, fixture.Included)
+				}
+			}
+			{
+				result := fixture.Range.String()
+				if result != fixture.RangeString {
+					t.Fatalf("version range string doesn't match the expected result: got %s (wanted %s)", result, fixture.RangeString)
+				}
+			}
+		})
+	}
+}
+
+type versionRangeMarshalingFixture struct {
+	JSON                      string
+	Range                     datatype.VersionRange
+	ExpectedUnmarshalingError bool
+}
+
+var versionRangeMarshalingFixtures = []versionRangeMarshalingFixture{
+	{Range: datatype.VersionRange{}, JSON: `{}`},
+	{Range: datatype.VersionRange{}, JSON: `{"start":"1","after":"1","end":"3"}`, ExpectedUnmarshalingError: true},
+	{Range: datatype.VersionRange{}, JSON: `{"start":"1","before":"2","end":"3"}`, ExpectedUnmarshalingError: true},
+	{Range: datatype.VersionRange{}, JSON: `5`, ExpectedUnmarshalingError: true},
+	{Range: datatype.VersionRange{Start: "1", End: "2"}, JSON: `{"start":"1","end":"2"}`},
+	{Range: datatype.VersionRange{Start: "0.1", End: "2.1.1"}, JSON: `{"start":"0.1","end":"2.1.1"}`},
+	{Range: datatype.VersionRange{Start: "1.0", StartIsExclusive: true, End: "4"}, JSON: `{"after":"1.0","end":"4"}`},
+	{Range: datatype.VersionRange{Start: "4", StartIsExclusive: true, End: "5.8.2B", EndIsExclusive: true}, JSON: `{"after":"4","before":"5.8.2B"}`},
+	{Range: datatype.VersionRange{Start: "v21.5.25128", End: "v21.6.27197", EndIsExclusive: true}, JSON: `{"start":"v21.5.25128","before":"v21.6.27197"}`},
+}
+
+func TestVersionRangeMarshaling(t *testing.T) {
+	for i, fixture := range versionRangeMarshalingFixtures {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			// Unmarshaling.
+			var unmarshaled datatype.VersionRange
+			if err := json.Unmarshal([]byte(fixture.JSON), &unmarshaled); err != nil {
+				if fixture.ExpectedUnmarshalingError {
+					return
+				} else {
+					t.Fatalf("unmarshaling %q encountered an unexpected error: %v", fixture.JSON, err)
+				}
+			} else if fixture.ExpectedUnmarshalingError {
+				t.Fatalf("unmarshaling %q did not produce an error as expected", fixture.JSON)
+			}
+
+			if unmarshaled != fixture.Range {
+				t.Fatalf("unmarshaled version range doesn't match the expected result: got \"%#v\" (wanted \"%#v\")", unmarshaled, fixture.Range)
+			}
+
+			// Marshaling.
+			marshaled, err := json.Marshal(fixture.Range)
+			if err != nil {
+				t.Fatalf("marshaling \"%#v\" encountered an unexpected error: %v", fixture.Range, err)
+			}
+
+			if string(marshaled) != fixture.JSON {
+				t.Fatalf("marshaled version range doesn't match the expected result: got %s (wanted %s)", string(marshaled), fixture.JSON)
+			}
+		})
+	}
+}
