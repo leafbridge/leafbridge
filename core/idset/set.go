ab54610c352cf622ddb61a36921bb5e270ef9688
Joshua Sjoding <ga@josh.hey.nu>
2025-08-19 20:38:12 -0700

    core/idset: Added SetOf[T].List() and added empty set check

--- core/idset/set.go
+++ core/idset/set.go
@@ -1,10 +1,18 @@
 package idset
 
+import (
+	"cmp"
+	"slices"
+)
+
 // SetOf is a set of IDs of type T.
-type SetOf[T comparable] map[T]struct{}
+type SetOf[T cmp.Ordered] map[T]struct{}
 
 // Contains returns true if the given id is present in the set.
 func (set SetOf[T]) Contains(id T) bool {
+	if len(set) == 0 {
+		return false
+	}
 	_, present := set[id]
 	return present
 }
@@ -19,4 +27,21 @@ func (set SetOf[T]) Add(id T) {
 // no action.
 func (set SetOf[T]) Remove(id T) {
 	delete(set, id)
+}
+
+// List returns the members of the set in an ordered list with ascending
+// order.
+func (set SetOf[T]) List() []T {
+	if len(set) == 0 {
+		return nil
+	}
+
+	items := make([]T, 0, len(set))
+	for item := range set {
+		items = append(items, item)
+	}
+
+	slices.Sort(items)
+
+	return items
 }
