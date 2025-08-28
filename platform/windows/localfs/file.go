c853784a5d1c5c1f9b385db0e535a90f519ce803
Joshua Sjoding <ga@josh.hey.nu>
2025-08-28 02:56:29 -0700

    platform/windows/localfs: Updated OpenFile() to properly handle symbolic links

--- platform/windows/localfs/file.go
+++ platform/windows/localfs/file.go
@@ -2,6 +2,7 @@ package localfs
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -24,39 +25,88 @@ func OpenFile(ref lbdeploy.FileRef) (File, error) {
 	// Start to build up the path of the file.
 	path := ref.Root.Path
 
-	// Open the known folder as our root directory.
+	// Open the known folder as our first root directory.
 	root, err := os.OpenRoot(ref.Root.Path)
 	if err != nil {
-		return File{}, err
+		return File{}, fmt.Errorf("failed to open known folder root path \"%s\": %w", ref.Root.Path, err)
 	}
+	defer func() {
+		// This is an intentional late evaluation of the root variable.
+		root.Close()
+	}()
 
 	// Traverse subdirectories, if present.
 	for _, next := range ref.Lineage {
+		// Hold a copy of the parent's path in case we need to traverse
+		// a symlink.
+		parentPath := path
+
 		// Continue buliding up the path of the file.
-		localized, err := filepath.Localize(next.Path)
+		nextPathLocalized, err := filepath.Localize(next.Path)
 		if err != nil {
-			return File{}, err
+			return File{}, fmt.Errorf("failed to localize subdirectory path for \"%s\": %w", parentPath, err)
 		}
-		path = filepath.Join(path, localized)
+		path = filepath.Join(path, nextPathLocalized)
 
 		// Hold a reference to the parent so that we can close it in a moment.
 		parent := root
 
-		// Traverse down to the next descendent.
-		root, err = parent.OpenRoot(next.Path)
-
-		// Always close the parent directory's file handle.
-		parent.Close()
-
-		// Stop if we were unable to traverse down.
+		// Lstat the next directory so that we can determine whether it is a
+		// symlink or not.
+		fi, err := parent.Lstat(nextPathLocalized)
 		if err != nil {
-			return File{}, err
+			return File{}, fmt.Errorf("failed to examine subdirectory path \"%s\": %w", path, err)
+		}
+
+		if fi.Mode()&os.ModeSymlink == 0 {
+			// Handle regular directories.
+
+			// Make sure we weren't expecting a symlink.
+			if next.Symlink {
+				return File{}, fmt.Errorf("expected a symlink at path: %s", path)
+			}
+
+			// Traverse down to the next descendent.
+			nextRoot, err := parent.OpenRoot(nextPathLocalized)
+			if err != nil {
+				return File{}, fmt.Errorf("failed to open directory \"%s\": %w", path, err)
+			}
+			root = nextRoot
+
+			// Always close the parent directory's file handle.
+			parent.Close()
+		} else {
+			// Handle symlinks.
+
+			// Make sure we were expecting a symlink.
+			if !next.Symlink {
+				return File{}, fmt.Errorf("encountered an unexpected symlink: %s", path)
+			}
+
+			// Read the destination.
+			destination, err := parent.Readlink(nextPathLocalized)
+			if err != nil {
+				return File{}, fmt.Errorf("failed to read symlink \"%s\": %w", path, err)
+			}
+
+			// Attempt to create a relative path to the destination from
+			// the parent.
+			relativeDestination, err := filepath.Rel(parentPath, destination)
+			if err != nil {
+				return File{}, fmt.Errorf("failed to build a relative path from symlink \"%s\" to \"%s\": %w", path, destination, err)
+			}
+
+			// Traverse down to the next descendent.
+			nextRoot, err := parent.OpenRoot(relativeDestination)
+			if err != nil {
+				return File{}, fmt.Errorf("failed to open directory \"%s\": %w", path, err)
+			}
+			root = nextRoot
+
+			// Always close the parent directory's file handle.
+			parent.Close()
 		}
 	}
-
-	// Now that we have the parent directory, but sure to close it when we're
-	// finished.
-	defer root.Close()
 
 	// Finish constrution of the file's path.
 	{
